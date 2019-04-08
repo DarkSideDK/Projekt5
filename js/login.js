d1064c3c530d16e3c5f661ef9bcaa02532c9c11a
Lone <lonecopy1@gmail.com>
2019-04-08 13:11:53 +0200

    Fixed redirect
    Ooops

--- js/login.js
+++ js/login.js
@@ -1,22 +1,23 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "Henrik Grove" && password == "hoc"){
-alert ("Godkendt!");
-window.location = "success.html"; // Redirecting to other page.
-return false;
-}
-else{
-attempt --;// Decrementing by one.
-alert("Forkert kodeord. Du har "+attempt+" forsøg tilbage.");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Henrik Grove" && password == "hoc") {
+        alert ("Godkendt!");
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    }
+
+    else {
+        attempt --;// Decrementing by one.
+        alert("Forkert kodeord. Du har "+attempt+" forsøg tilbage.");
+        // Disabling fields after 3 attempts.
+        if ( attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
 }
