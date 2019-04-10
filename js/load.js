705c13e832bd33c5f75e6c24a58c116392d41342
Lone <lonecopy1@gmail.com>
2019-04-10 18:05:59 +0200

    Rightbox optimering, css stylesheet
    - Rettet rightbox til så den kan indeholde andre filer. Desværre ikke god nok endnu
    - Optimeret css fil, flyttet rundt på elementer

--- js/load.js
+++ js/load.js
@@ -1,3 +1,12 @@
-function load() {
-     document.getElementById("rightbox").innerHTML='<object type="text/html" data="udviklingssamtale.html" ></object>';
+var height;
+var content;
+
+function loadUdviklingssamtale() {
+    /*Indsæt højre vindue*/
+    document.getElementById("rightbox").innerHTML='<object type="text/html" id="window" data="udviklingssamtale.html"></object>';
+    
+    /*Højre vindue skal være sammme højde som venstre vindue */
+    height = document.getElementById('leftbox').scrollHeight;
+    document.getElementById('window').style.height = height + "px";
+    
 }
