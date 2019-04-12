fa2e613add32b02526ff938b36ccfdbd32dea481
IffyComet <47402722+IffyComet@users.noreply.github.com>
2019-04-12 21:48:28 +0200

    noget med at gøre knapperne grønne stadig

--- js/load.js
+++ js/load.js
@@ -2,10 +2,18 @@ var height;
 var content;
 
 function makeGray() {
-    document.getElementById("udvknap").style.backgroundColor = "white";
-    document.getElementById("udvknap").style.border = "4px solid #c0bcbb";
-    document.getElementById("udvknap").style.position = "relative";
-    document.getElementById("udvknap").style.zIndex = "0";
+    var udvknap = document.getElementById("udvknap");
+    var kollegaknap = document.getElementById("kollegaknap");
+    
+    udvknap.style.backgroundColor = "white";
+    udvknap.style.border = "4px solid #c0bcbb";
+    udvknap.style.position = "relative";
+    udvknap.style.zIndex = "0";
+    kollegaknap.style.backgroundColor = "white";
+    kollegaknap.style.border = "4px solid #c0bcbb";
+    kollegaknap.style.position = "relative";
+    kollegaknap.style.zIndex = "0";
+    
 }
 
 function loadUdviklingssamtale() {
@@ -17,7 +25,6 @@ function loadUdviklingssamtale() {
     document.getElementById('window').style.height = height + "px";
     
     makeGray();
-    
     var udvknap = document.getElementById("udvknap");
     udvknap.style.backgroundColor = "rgba(166,187,117,0.1)";
     udvknap.style.border = "4px solid rgba(166,187,117,1)";
@@ -31,7 +38,10 @@ function loadKollega() {
     document.getElementById("rightbox").innerHTML = '<object type="text/html" id="window" data="#"></obejct>';
     
     makeGray();
-    
-    
+    var kollegaknap = document.getElementById("kollegaknap");
+    kollegaknap.style.backgroundColor = "rgba(166,187,117,0.1)";
+    kollegaknap.style.border = "4px solid rgba(166,187,117,1)";
+    kollegaknap.style.position = "relative";
+    kollegaknap.style.zIndex = "1";
     
 }
