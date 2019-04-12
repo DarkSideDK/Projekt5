e5c48bd43e79f6fac056bb7179ad4970ba256682
IffyComet <47402722+IffyComet@users.noreply.github.com>
2019-04-12 14:56:28 +0200

    stadig knapper der skal være grønne

--- js/load.js
+++ js/load.js
@@ -1,6 +1,13 @@
 var height;
 var content;
 
+function makeGray() {
+    document.getElementById("udvknap").style.backgroundColor = "white";
+    document.getElementById("udvknap").style.border = "4px solid #c0bcbb";
+    document.getElementById("udvknap").style.position = "relative";
+    document.getElementById("udvknap").style.zIndex = "0";
+}
+
 function loadUdviklingssamtale() {
     /*Indsæt højre vindue*/
     document.getElementById("rightbox").innerHTML='<object type="text/html" id="window" data="udviklingssamtale.html"></object>';
@@ -9,10 +16,22 @@ function loadUdviklingssamtale() {
     height = document.getElementById('leftbox').scrollHeight;
     document.getElementById('window').style.height = height + "px";
     
-    document.getElementById("udvknap").style.backgroundColor = "rgba(166,187,117,0.1)";
-    document.getElementById("udvknap").style.border = "4px solid rgba(166,187,117,1)";
-    document.getElementById("udvknap").style.position = "relative";
-    document.getElementById("udvknap").style.zIndex = "1";
-    document.getElementById("udvknap").style.color = "rgba(166,187,117,1)";
-    document.getElementById("udvknap").style
+    makeGray();
+    
+    var udvknap = document.getElementById("udvknap");
+    udvknap.style.backgroundColor = "rgba(166,187,117,0.1)";
+    udvknap.style.border = "4px solid rgba(166,187,117,1)";
+    udvknap.style.position = "relative";
+    udvknap.style.zIndex = "1";
+    
+    var knaptekst = document.getElementById("udvknap").document.getElementsByClassName("box0")[1];
+}
+
+function loadKollega() {
+    document.getElementById("rightbox").innerHTML = '<object type="text/html" id="window" data="#"></obejct>';
+    
+    makeGray();
+    
+    
+    
 }
